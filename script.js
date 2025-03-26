9adc7bf563edfae44670379cbd01a43837e907e7
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2025-03-27 00:09:26 +0700

    js update

--- script.js
+++ script.js
@@ -6,9 +6,8 @@ document.getElementById("search").addEventListener("input", debounce(function ()
         let text = item.textContent.toLowerCase();
         item.style.display = text.includes(query) ? "flex" : "none";
     });
-}, 300)); // Debounce dengan delay 300ms
+}, 300));
 
-// Fungsi debounce untuk mencegah eksekusi berulang terlalu cepat
 function debounce(func, delay) {
     let timeout;
     return function () {
